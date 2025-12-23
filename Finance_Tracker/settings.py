3da394a938a11704c3744e40b79ab341a13932f3
Nibedita-Mohapatro <nibeditamohapatro123@gmail.com>
2025-12-24 00:32:30 +0530

    Deploy Django on Vercel

--- Finance_Tracker/settings.py
+++ Finance_Tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--7sbh#5nq&t7$)6%4+)!31*7@&655egb$ek2_zuof(c6hwdx@-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
