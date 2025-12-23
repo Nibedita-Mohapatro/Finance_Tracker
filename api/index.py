3b1be72bbf8e96269a47bf6a9a256336e863fa4e
Nibedita-Mohapatro <nibeditamohapatro123@gmail.com>
2025-12-24 00:49:13 +0530

    Django on Vercel

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "projectname.settings"
+    "Finance_Tracker.settings"
 )
 
 app = get_wsgi_application()
