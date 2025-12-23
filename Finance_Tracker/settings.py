788b40a2c940679175f7725826463a6e7a2786e3
Nibedita-Mohapatro <nibeditamohapatro123@gmail.com>
2025-12-24 00:59:26 +0530

    fixed csrf for  Vercel

--- Finance_Tracker/settings.py
+++ Finance_Tracker/settings.py
@@ -122,3 +122,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.vercel.app"
+]
